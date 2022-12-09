c79af334f47de63042cf978acefae9c7c86dfa9e
Tripp Barker <colemanbarker@gmail.com>
2022-12-08 20:48:14 -0500

    working on sprite movement

--- src/spooky.js
+++ src/spooky.js
@@ -16,6 +16,16 @@ c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width = cWidth, canvas.height = cHeight);
 
 
+// PLAYER COMPONENTS
+let moving = false;
+let lastPlayerDirection = "s";
+let playerSpeed = elementSize / 12;
+let spriteFrame = 0;
+
+
+// GAME MECHANIC COMPONENTS
+let playing = true;
+
 // Asset Classes
 
 class Map{
@@ -30,6 +40,24 @@ class Map{
 }
 
 class PlayerObj{
+    constructor(playerImg){
+        this.image = new Image();
+        this.image.src = playerImg;
+    }
+
+    draw(){
+        c.drawImage(
+            this.image,
+            elementSize * spriteFrame,
+            0,
+            elementSize,
+            elementSize, 
+            cWidth / 2 - (elementSize/2),
+            cHeight / 2 - (elementSize/2),
+            elementSize,
+            elementSize
+            );
+    }
 
 }
 
@@ -40,4 +68,47 @@ class BoundaryObj{
 // Object Creation
 
 let basicMap = new Map('../res/maps/basicMap.png');
-basicMap.draw();
+
+let player = new PlayerObj('../res/sprites/ghostie/ghostie.png');
+
+// Function
+
+function move(e){
+    moving = true;
+    lastPlayerDirection = e.key;
+}
+
+function stopMove(e){
+    moving = false;
+}
+
+function redraw(){
+    if (moving){
+        switch (lastPlayerDirection){
+            case 'w':
+                offset.y += playerSpeed;
+                spriteFrame = 1;
+                break;
+            case 'a':
+                offset.x += playerSpeed;
+                spriteFrame = 3;
+                break;
+            case 's':
+                offset.y -= playerSpeed;
+                spriteFrame = 0;
+                break;
+            case 'd':
+                offset.x -= playerSpeed;
+                spriteFrame = 2;
+                break;
+        }
+    }
+    basicMap.draw();
+    player.draw();
+}
+
+// Event Listeners
+
+window.addEventListener('keydown', move);
+window.addEventListener('keyup', stopMove);
+window.setInterval(redraw, 35);
